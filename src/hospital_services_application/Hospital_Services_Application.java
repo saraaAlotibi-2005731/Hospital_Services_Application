98787fe59b26221ca5588c9ee2c0a73c47c71708
razan_almalki <razansa12@outlook.sa>
2023-01-28 20:53:55 +0300

    Razan almalki

--- src/hospital_services_application/Hospital_Services_Application.java
+++ src/hospital_services_application/Hospital_Services_Application.java
@@ -1,12 +1,5 @@
 package hospital_services_application;
-/**
- *
- * @author SaraaTurki
- */
 public class Hospital_Services_Application {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {    
     }
-    
 }
