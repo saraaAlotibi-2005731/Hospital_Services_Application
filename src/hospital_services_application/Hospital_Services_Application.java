9dcf848e8fda2ccb4f9739d8f443b787a9a6c033
SaraaTurki <ssalemalotibi@stu.kau.edu.sa>
2023-01-28 19:07:46 +0300

    Saraa commit

---
+++ src/hospital_services_application/Hospital_Services_Application.java
@@ -0,0 +1,12 @@
+package hospital_services_application;
+/**
+ *
+ * @author SaraaTurki
+ */
+public class Hospital_Services_Application {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
